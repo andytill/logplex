43a4ca6e7b12b4d336f9ea0eef148f3a24f1587b
Alex Arnell <alex@heroku.com>
2014-06-27 11:55:14 -0700

    upgrade live_upgrade script

--- upgrades/v72.1_v73/live_upgrade.erl
+++ upgrades/v72.1_v73/live_upgrade.erl
@@ -1,6 +1,6 @@
 f(DowngradeNode).
 DowngradeNode = fun () ->
-    CurVsn = "v73",
+    CurVsn = "firehose-spike",
     NextVsn = "v72.1",
     case logplex_app:config(git_branch) of
         CurVsn ->
@@ -23,15 +23,19 @@ DowngradeNode = fun () ->
     {module, logplex_worker} = l(logplex_worker),
     {module, nsync_callback} = l(nsync_callback),
 
+    application:set_env(logplex, firehose_channel_ids, ""),
+    ets:delete(firehose),
+
     io:format(whereis(user), "at=upgrade_end cur_vsn=~p~n", [NextVsn]),
     ok = application:set_env(logplex, git_branch, NextVsn),
     ok
 end.
 
+
 f(UpgradeNode).
 UpgradeNode = fun () ->
-    CurVsn = "v72.1",
-    NextVsn = "v73",
+    CurVsn = "v73",
+    NextVsn = "firehose-spike",
     case logplex_app:config(git_branch) of
         CurVsn ->
             io:format(whereis(user), "at=upgrade_start cur_vsn=~p~n", [tl(CurVsn)]);
@@ -55,9 +59,14 @@ UpgradeNode = fun () ->
     {module, nsync_callback} = l(nsync_callback),
 
     spawn(fun () ->
-        ets:new(firehose,
+        ets:new(firehose_master,
                 [named_table, public, set,
                  {keypos, 2},
+                 {read_concurrency, true},
+                 {heir, whereis(logplex_db), undefined}]),
+        ets:new(firehose_workers,
+                [named_table, public, set,
+                 {keypos, 1},
                  {read_concurrency, true},
                  {heir, whereis(logplex_db), undefined}])
     end),
@@ -66,7 +75,6 @@ UpgradeNode = fun () ->
     ok = application:set_env(logplex, git_branch, NextVsn),
     ok
 end.
-
 f(NodeVersions).
 NodeVersions = fun () ->
     lists:keysort(3,
@@ -104,6 +112,26 @@ RollingUpgrade = fun (Nodes) ->
         {good, [N | Upgraded]};
       Else ->
         {{bad, N, Else}, Upgraded}
+    end;
+    (N, {_, _} = Acc) -> Acc
+    end,
+    {good, []},
+    Nodes)
+end.
+
+
+Activate = fun () ->
+    application:set_env(logplex, firehose_channel_ids, "5586339,5586340,5586399"),
+    {shard_pool,master_shard,3,{5586339,5586340,5586399}} = logplex_firehose:read_and_store_master_info(),
+    ok
+end.
+RollingActivate = fun (Nodes) ->
+  lists:foldl(fun (N, {good, Activated}) ->
+    case rpc:call(N, erlang, apply, [ Activate, [] ]) of
+      ok ->
+        {good, [N | Activated]};
+      Else ->
+        {{bad, N, Else}, Activated}
     end;
     (N, {_, _} = Acc) -> Acc
     end,
